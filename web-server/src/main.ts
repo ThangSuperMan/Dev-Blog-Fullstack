4cd1c05ef78b87f862d39ccf550c1cb554a1c3f0
thangsuperman <thangjenny2002@gmail.com>
2023-07-01 12:07:02 +0700

    feat: migrate from express to fastify server

--- web-server/src/main.ts
+++ web-server/src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+// import * as cookieParser from 'cookie-parser';
+import fastifyCookie from '@fastify/cookie';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
+  // const app = await NestFactory.create(AppModule);
+  // app.use(cookieParser());
+  // await app.listen(process.env.APP_PORT);
 
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.register(fastifyCookie);
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
